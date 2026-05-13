7da15242c9b76004e6f10b54cdf45eeef746a50f
Yorik27 <yorik27870@gmail.com>
2026-05-13 15:57:11 +0200

    Force désinstallation SW au chargement

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker
-      .register("sw.js")
-      .then(serviceWorker => {
-        console.log("Service Worker registered: ", serviceWorker);
-      })
-      .catch(error => {
-        console.error("Error registering the Service Worker: ", error);
-      });
-  }
+if ('serviceWorker' in navigator) {
+    // Désinscrire tous les SW existants puis réinscrire le nouveau
+    navigator.serviceWorker.getRegistrations().then(regs => {
+        const unregisterAll = regs.map(reg => reg.unregister());
+        return Promise.all(unregisterAll);
+    }).then(() => {
+        // Réinscrire le SW propre
+        return navigator.serviceWorker.register('sw.js');
+    }).then(reg => {
+        console.log('Service Worker réinscrit :', reg);
+        reg.update();
+    }).catch(err => {
+        console.error('Erreur SW :', err);
+    });
+}
