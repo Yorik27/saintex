9a813b8030bc43edf182cfdbd4e34b71231cc316
Yorik27 <yorik27870@gmail.com>
2026-05-26 16:23:07 +0200

    Simplification app.js - suppression boucle SW

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 if ('serviceWorker' in navigator) {
-    // Désinscrire tous les SW existants puis réinscrire le nouveau
-    navigator.serviceWorker.getRegistrations().then(regs => {
-        const unregisterAll = regs.map(reg => reg.unregister());
-        return Promise.all(unregisterAll);
-    }).then(() => {
-        // Réinscrire le SW propre
-        return navigator.serviceWorker.register('sw.js');
-    }).then(reg => {
-        console.log('Service Worker réinscrit :', reg);
-        reg.update();
-    }).catch(err => {
+    navigator.serviceWorker.register('sw.js')
+    .then(reg => {
+        console.log('Service Worker enregistré :', reg.scope);
+    })
+    .catch(err => {
         console.error('Erreur SW :', err);
     });
 }
